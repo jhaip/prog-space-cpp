faf12b276303de75bfdbf945cc60978eb3be7dc2
Jacob Haip <jhaip@mit.edu>
2022-05-10 23:07:27 -0400

    add second window for calibrations

--- main.cpp
+++ main.cpp
@@ -178,8 +178,11 @@ int main () {
         sf::Image latestFrameImage;
         sf::Texture latestFrameTexture;
         sf::Sprite latestFrameSprite;
+        std::vector<std::pair<float, float>> calibration = {{10, 10}, {1200, 10}, {1200, 700}, {10, 700}};
+        int calibrationCorner = 0;
 
         sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+        sf::RenderWindow debugWindow(sf::VideoMode(1280, 720), "debug");
         sf::Font font;
         // TODO: find font into project and load that
         if (!font.loadFromFile("/System/Library/Fonts/Supplemental/Arial.ttf")) {
@@ -200,7 +203,7 @@ int main () {
 
         int loopCount = 0;
         window.setFramerateLimit(60);
-        while (window.isOpen())
+        while (window.isOpen() && debugWindow.isOpen())
         {
             // currentTime = clock.getElapsedTime();
             // if (currentTime.asSeconds() - previousTime.asSeconds() > 1.0f/60.0f) {
@@ -260,8 +263,49 @@ int main () {
                         }
                     }
                 }
+                while (debugWindow.pollEvent(event))
+                {
+                    if (event.type == sf::Event::Closed)
+                    {
+                        debugWindow.close();
+                    }
+                    else if (event.type == sf::Event::KeyPressed)
+                    {
+                        if (event.key.code == sf::Keyboard::Enter)
+                        {
+                            std::cout << "Debug Enter pressed" << std::endl;
+                            db.print();
+                        }
+                    }
+                }
 
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1)) {
+                    calibrationCorner = 0;
+                } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num2)) {
+                    calibrationCorner = 1;
+                } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num3)) {
+                    calibrationCorner = 2;
+                } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num4)) {
+                    calibrationCorner = 3;
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+                {
+                    calibration[calibrationCorner].first++;
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+                {
+                    calibration[calibrationCorner].first--;
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+                {
+                    calibration[calibrationCorner].second--;
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+                {
+                    calibration[calibrationCorner].second++;
+                }
+
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num9)) {
                     script4();
                 } else {
                     db.cleanup("5");
@@ -341,6 +385,27 @@ int main () {
 
 
                 window.display();
+
+                debugWindow.clear();
+                debugWindow.draw(latestFrameSprite);
+                int cornerIndex = 0;
+                sf::VertexArray verticesOfCalibration(sf::LinesStrip, 5);
+                for (const auto &corner : calibration) {
+                    sf::CircleShape circle(10);
+                    circle.setPosition(sf::Vector2f(corner.first-10.f, corner.second-10.f));
+                    if (cornerIndex == calibrationCorner) {
+                        circle.setFillColor(sf::Color(255, 255, 0));
+                    } else {
+                        circle.setFillColor(sf::Color(0, 255, 0));
+                    }
+                    verticesOfCalibration[cornerIndex].position = sf::Vector2f(corner.first, corner.second);
+                    verticesOfCalibration[cornerIndex].color = sf::Color::Green;
+                    debugWindow.draw(circle);
+                    cornerIndex++;
+                }
+                verticesOfCalibration[4] = verticesOfCalibration[0];
+                debugWindow.draw(verticesOfCalibration);
+                debugWindow.display();
             // }
         }
 
